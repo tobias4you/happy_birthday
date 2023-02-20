7ed605c62a871d4b90a0825f36dd5070065dc832
tobias4you <72708989+tobias4you@users.noreply.github.com>
2023-02-20 13:27:20 +0100

    init: create birthday site for Tobias

---
+++ script.js
@@ -0,0 +1,8 @@
+navigator.mediaDevices.getUserMedia({ audio: true }).then(function (stream) {
+        
+    var x = document.getElementById("myAudio"); 
+    x.play();
+  
+     // stop microphone stream acquired by getUserMedia
+     stream.getTracks().forEach(function (track) { track.stop(); });
+  });
